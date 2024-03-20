731129ee4585b8cb3e854117f807e4ec8d26f957
zerocodde <163998317+zerocodde@users.noreply.github.com>
2024-03-20 15:26:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -32,17 +32,13 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/zerocodde)
- 
- I am Super noob Please Support My Hard Work.
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [ZeroCodde](https://telegram.me/zerocodde)
 )
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
 
-How to Use Bot & it's Benefits??
+*CURRENTLY WORKING FOR @DINOMOVIESTG*
 
 📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
-
-⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
 """
